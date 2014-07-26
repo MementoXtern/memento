7d4ccdb509e2a11824b4a8879b7224f09c830301
unknown <BPorter@IN5580.ET.LOCAL>
2014-07-26 15:51:53 -0400

    Added Dynamic menu mockup

---
+++ Lib/BusinessObjects/MenuItemObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lib.BusinessObjects
+{
+    public class MenuItemObject
+    {
+        public string Url { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
--- WebApp/Site.Master.cs
+++ WebApp/Site.Master.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Lib.BusinessObjects;
+using MenuItem = Lib.DataObjects.MenuItem;
 namespace WebApp
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
+        public List<MenuItemObject> _menuItems = new List<MenuItemObject>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            Session["AccountId"] = 5;
+            Session["AccountTypeId"] = 2;
 
+            if (Session["AccountId"] != null && Session["AccountTypeId"] != null)
+            {
+                SetMenu();
+            }
+            else
+            {
+                Response.Redirect("Default.aspx");
+            }
+        }
+
+        private void SetMenu()
+        {
+            int accountId = (int)Session["AccountId"];
+            int typeId = (int)Session["AccountTypeId"];
+
+            _menuItems = MenuItem.GetMenuItems(typeId);
         }
     }
 }
